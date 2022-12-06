28584c6aad6296f6c0178c9c389836b6998d4ae9
Rodrigo Regalado <r.regalado6204@gmail.com>
2022-12-05 19:40:15 -0800

    added delete route

--- views/show.jsx
+++ views/show.jsx
@@ -1,7 +1,7 @@
 const React = require ('react')
 const Default = require('./layouts/default')
 
-function Show ({bread}) {
+function Show ({bread, index}) {
     return (
         <Default>
             <h2> Show Page</h2>
@@ -17,6 +17,11 @@ function Show ({bread}) {
             </p>
             <img src={bread.image} alt={bread.name} />
             <li><a href="/breads">Go Home</a></li>
+            <form action={`/breads/${index}?_method=DELETE`} method="POST">
+                <input type='submit' value="DELETE"/>
+            </form>
+
+
         </Default>
     )
 }
